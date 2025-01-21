78c56ed4bb0a0f7fe3e939e45be9d2f9495c3409
Ahmed Marzook <m4marzook@gmail.com>
2025-01-21 22:27:34 +0000

    Github Pages Config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+// vite.config.js
+import react from "@vitejs/plugin-react-swc";
 
-// https://vite.dev/config/
+import { defineConfig } from "vite";
+
+import { ghPages } from "vite-plugin-gh-pages";
+
+import fs from "node:fs";
+import path from "node:path";
+
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    ghPages({
+      // Add hook to persist CNAME file during deployment
+      onBeforePublish: ({ outDir }) => {
+        const CNAME = path.join(outDir, "CNAME");
+        fs.writeFileSync(CNAME, "lastlang.kaizenflow.dev"); // Replace with your domain
+      },
+    }),
+  ],
+  base: "/", // Base URL for custom domain
+});
